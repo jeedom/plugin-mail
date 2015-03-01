eaf5ba4cd8309640fbbf3df97809d0873a14a240
Loic <loicgevrey@gmail.com>
2015-03-01 13:55:22 +0100

    Bugfix

--- core/class/mail.class.php
+++ core/class/mail.class.php
@@ -21,90 +21,84 @@ require_once dirname(__FILE__) . '/../../core/php/mail.inc.php';
 
 class mail extends eqLogic {
 
-    public function preSave() {
-        $this->setIsVisible(0);
-    }
-
 }
 
 class mailCmd extends cmd {
-    /*     * *************************Attributs****************************** */
+	/*     * *************************Attributs****************************** */
 
+	/*     * ***********************Methode static*************************** */
 
-    /*     * ***********************Methode static*************************** */
+	/*     * *********************Methode d'instance************************* */
 
+	public function preSave() {
+		$this->setType('action');
+		$this->setSubType('message');
+		if ($this->getConfiguration('recipient') == '') {
+			throw new Exception(__('L\'adresse mail ne peut etre vide', __FILE__));
+		}
+		if (filter_var($this->getConfiguration('recipient'), FILTER_VALIDATE_EMAIL) === false) {
+			throw new Exception(__('L\'adresse mail n\'est pas valide', __FILE__));
+		}
+	}
 
-    /*     * *********************Methode d'instance************************* */
+	public function execute($_options = null) {
+		$eqLogic = $this->getEqLogic();
+		if ($_options === null) {
+			throw new Exception(__('[Mail] Les options de la fonction ne peuvent etre null', __FILE__));
+		}
 
-    public function preSave() {
-        $this->setType('action');
-        $this->setSubType('message');
-        if ($this->getConfiguration('recipient') == '') {
-            throw new Exception(__('L\'adresse mail ne peut etre vide', __FILE__));
-        }
-        if (filter_var($this->getConfiguration('recipient'), FILTER_VALIDATE_EMAIL) === false) {
-            throw new Exception(__('L\'adresse mail n\'est pas valide', __FILE__));
-        }
-    }
+		if ($_options['message'] == '' && $_options['title'] == '') {
+			throw new Exception(__('[Mail] Le message et le sujet ne peuvent être vide', __FILE__));
+			return false;
+		}
 
-    public function execute($_options = null) {
-        $eqLogic = $this->getEqLogic();
-        if ($_options === null) {
-            throw new Exception(__('[Mail] Les options de la fonction ne peuvent etre null', __FILE__));
-        }
+		if ($_options['title'] == '') {
+			$_options['title'] = __('[Jeedom] - Notification', __FILE__);
+		}
 
-        if ($_options['message'] == '' && $_options['title'] == '') {
-            throw new Exception(__('[Mail] Le message et le sujet ne peuvent être vide', __FILE__));
-            return false;
-        }
+		$mail = new PHPMailer(true); //PHPMailer instance with exceptions enabled
+		$mail->CharSet = 'utf-8';
+		$mail->SMTPDebug = 0;
+		switch ($eqLogic->getConfiguration('sendMode', 'mail')) {
+			case 'smtp':
+				$mail->isSMTP();
+				$mail->Host = $eqLogic->getConfiguration('smtp::server');
+				$mail->Port = (integer) $eqLogic->getConfiguration('smtp::port');
+				if ($eqLogic->getConfiguration('smtp::security', '') != '' && $eqLogic->getConfiguration('smtp::security', '') != 'none') {
+					$mail->SMTPSecure = $eqLogic->getConfiguration('smtp::security', '');
+				}
+				if ($eqLogic->getConfiguration('smtp::username') != '') {
+					$mail->SMTPAuth = true;
+					$mail->Username = $eqLogic->getConfiguration('smtp::username'); 	// SMTP account username
+					$mail->Password = $eqLogic->getConfiguration('smtp::password'); 	// SMTP account password
+				}
+				break;
+			case 'mail':
+				$mail->isMail();
+				break;
+			case 'sendmail':
+				$mail->isSendmail();
+				break;
+			case 'qmail':
+				$mail->isQmail();
+				break;
+			default:
+				throw new Exception(__('Mode d\'envoi non reconnu', __FILE__));
+		}
+		if ($eqLogic->getConfiguration('fromName') != '') {
+			$mail->addReplyTo($eqLogic->getConfiguration('fromMail'), $eqLogic->getConfiguration('fromName'));
+			$mail->FromName = $eqLogic->getConfiguration('fromName');
+		} else {
+			$mail->addReplyTo($eqLogic->getConfiguration('fromMail'));
+			$mail->FromName = $eqLogic->getConfiguration('fromMail');
+		}
+		$mail->From = $eqLogic->getConfiguration('fromMail');
+		$mail->isHTML(true);
+		$mail->addAddress($this->getConfiguration('recipient'));
+		$mail->Subject = $_options['title'];
+		$mail->Body = nl2br($_options['message']);
+		return $mail->send();
+	}
 
-        if ($_options['title'] == '') {
-            $_options['title'] = __('[Jeedom] - Notification', __FILE__);
-        }
-
-        $mail = new PHPMailer(true);  //PHPMailer instance with exceptions enabled
-        $mail->CharSet = 'utf-8';
-        $mail->SMTPDebug = 0;
-        switch ($eqLogic->getConfiguration('sendMode', 'mail')) {
-            case 'smtp':
-                $mail->isSMTP();
-                $mail->Host = $eqLogic->getConfiguration('smtp::server');
-                $mail->Port = (integer) $eqLogic->getConfiguration('smtp::port');
-                if ($eqLogic->getConfiguration('smtp::security', '') != '' && $eqLogic->getConfiguration('smtp::security', '') != 'none') {
-                    $mail->SMTPSecure = $eqLogic->getConfiguration('smtp::security', '');
-                }
-                if ($eqLogic->getConfiguration('smtp::username') != '') {
-                    $mail->SMTPAuth = true;
-                    $mail->Username = $eqLogic->getConfiguration('smtp::username'); // SMTP account username
-                    $mail->Password = $eqLogic->getConfiguration('smtp::password'); // SMTP account password
-                }
-                break;
-            case 'mail':
-                $mail->isMail();
-                break;
-            case 'sendmail':
-                $mail->isSendmail();
-                break;
-            case 'qmail':
-                $mail->isQmail();
-                break;
-            default:
-                throw new Exception(__('Mode d\'envoi non reconnu', __FILE__));
-        }
-        if ($eqLogic->getConfiguration('fromName') != '') {
-            $mail->addReplyTo($eqLogic->getConfiguration('fromMail'), $eqLogic->getConfiguration('fromName'));
-            $mail->FromName = $eqLogic->getConfiguration('fromName');
-        } else {
-            $mail->addReplyTo($eqLogic->getConfiguration('fromMail'));
-            $mail->FromName = $eqLogic->getConfiguration('fromMail');
-        }
-        $mail->From = $eqLogic->getConfiguration('fromMail');
-        $mail->isHTML(true);
-        $mail->addAddress($this->getConfiguration('recipient'));
-        $mail->Subject = $_options['title'];
-        $mail->Body = nl2br($_options['message']);
-        return $mail->send();
-    }
-
-    /*     * **********************Getteur Setteur*************************** */
+	/*     * **********************Getteur Setteur*************************** */
 }

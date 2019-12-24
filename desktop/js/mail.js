263e10164e3fef2dab4fbcbbbd3292be09f87a27
JEALG <jalain.gosse@icloud.com>
2019-12-24 12:26:39 +0100

    Migration font-awesome 5
    Suite au sujet
    https://community.jeedom.com/t/icone-certains-icones-nont-pas-ete-migrer-sur-la-font-awesome-5/11852
    
    + mise à jour de l'affichage pour la V4

--- desktop/js/mail.js
+++ desktop/js/mail.js
@@ -26,6 +26,9 @@ function addCmdToTable(_cmd) {
     }
     var tr = '<tr class="cmd" data-cmd_id="' + init(_cmd.id) + '">';
     tr += '<td>';
+    tr += '<span class="cmdAttr" data-l1key="id"></span>';
+    tr += '</td>';
+    tr += '<td>';
     tr += '<input class="cmdAttr form-control" data-l1key="id" style="display : none;">';
     tr += '<input class="cmdAttr form-control" data-l1key="name"></td>';
     tr += '<td><input class="cmdAttr form-control" data-l1key="configuration" data-l2key="recipient"></td>';
@@ -33,10 +36,10 @@ function addCmdToTable(_cmd) {
     tr += '<input class="cmdAttr form-control" data-l1key="type" value="action" style="display : none;">';
     tr += '<input class="cmdAttr form-control" data-l1key="subType" value="message" style="display : none;">';
     if (is_numeric(_cmd.id)) {
-        tr += '<a class="btn btn-default cmdAction" data-action="configure"><i class="fa fa-cogs"></i></a> ';
-        tr += '<a class="btn btn-default cmdAction" data-action="test"><i class="fa fa-rss"></i> {{Tester}}</a> ';
+        tr += '<a class="btn btn-default btn-xs cmdAction expertModeVisible" data-action="configure"><i class="fas fa-cogs"></i></a> ';
+        tr += '<a class="btn btn-default btn-xs cmdAction" data-action="test"><i class="fas fa-rss"></i> {{Tester}}</a>';
     }
-    tr += '<i class="fa fa-minus-circle cmdAction cursor" data-action="remove" style="margin-left:7px"></i></td>';
+    tr += '<i class="fas fa-minus-circle pull-right cmdAction cursor" data-action="remove"></i></td>';
     tr += '</tr>';
     $('#table_cmd tbody').append(tr);
     $('#table_cmd tbody tr:last').setValues(_cmd, '.cmdAttr');
